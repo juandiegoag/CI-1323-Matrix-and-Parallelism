31c6dc300632529722b26e65d6f835a2256658d0
Juan Diego Araya <juand.95820@gmail.com>
2016-04-03 15:09:59 -0600

    Procesos de apariciones en filas y suma de columnas

--- TareaProgramada.cc
+++ TareaProgramada.cc
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-int main ( int argc,  char *argv[] )
+int main (int argc,  char *argv[] )
 {
-  int id, numproc, * rbuf;
+  int id, numproc, *rbuf;
 
   MPI_Init( &argc , &argv );
   MPI_Comm_size( MPI_COMM_WORLD , &numproc );
@@ -15,8 +15,7 @@ int main ( int argc,  char *argv[] )
 
   int a = 0, filas = 0, columnas = 0;
 
-  if( id == 0 )
-  {
+  if( id == 0 ){
     //Desde el proceso 0 solicita los valores al usuario
 
     cout << "Ingrese el multiplo del numero de filas: " << endl;
@@ -30,42 +29,94 @@ int main ( int argc,  char *argv[] )
     int * matriz = ( int * ) malloc ( filas * columnas * sizeof( int ) );
 
     // Llenado de matriz desde proceso 0
-    for( int i = 0; i < filas; i++ )
-    {
-      for( int j = 0; j < columnas; j++ )
-      {
-         * ( matriz + i * columnas + j ) = rand() % 5;
+    for( int i = 0; i < filas; i++ ){
+      for( int j = 0; j < columnas; j++ ){
+         *( matriz + i * columnas + j ) = rand() % 5;
+         cout<<*( matriz + i * columnas + j )<<" ";
       }
+      cout<<endl;
     }
-
     // Repartición de la matriz entre los procesos pertenecientes al MPI_COMM_WORLD
     // a cada proceso se le repartirá a filas de c enteros ==> a * C
 
-    int cantidad = a * columnas
+    int cantidad = a * columnas;
     MPI_Scatter( matriz, cantidad, MPI_INT, rbuf, cantidad, MPI_INT ,0 ,MPI_COMM_WORLD);
 
+    int *conteoFilas  = (int*) malloc(5*a*sizeof(int));
+    int *sumaColumnas = (int*) malloc(columnas*sizeof(int));
+    int offset = 0;
 
-/*
-    for( int i = 0; i < filas; i++ )
-    {
-      for( int j = 0; j < columnas; j++ )
-      {
-         cout << * ( matriz + i * columnas + j ) << " ";
+    for (int j = 0; j < 5*a; j++) {
+      conteoFilas[j]=0;
+    }
+
+    for (int j = 0; j < columnas; j++) {
+      sumaColumnas[j]=0;
+    }
+
+    for (int i = 0; i < cantidad; i++) {
+      sumaColumnas[i%columnas]+=matriz[i];
+
+      if (i%columnas == 0 && i>0) {
+        offset+=5;
       }
 
-      cout << endl;
+      switch (matriz[i]) {
+        case 0:
+          *(conteoFilas+offset)+=1;
+        break;
+
+        case 1:
+          *(conteoFilas+offset+1)+=1;
+        break;
+
+        case 2:
+          *(conteoFilas+offset+2)+=1;
+        break;
+
+        case 3:
+          *(conteoFilas+offset+3)+=1;
+        break;
+
+        case 4:
+          *(conteoFilas+offset+4)+=1;
+        break;
+      }
+
     }
-  */
-}
-
-MPI_Bcast(&a, 1, MPI_INT, 0, MPI_COMM_WORLD);
-MPI_Bcast(&filas, 1, MPI_INT, 0, MPI_COMM_WORLD);
-MPI_Bcast(&columnas, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 
 
+  }
 
-  //MPI_Finalize();
+
+  MPI_Bcast(&a,        1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&filas,    1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&columnas, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+
+
+
+  MPI_Finalize();
   return 0;
 
 }
+
+/*
+    for( int i = 0; i < filas; i++ ){
+      for( int j = 0; j < columnas; j++ ){
+         cout << * ( matriz + i * columnas + j ) << " ";
+      }
+      cout << endl;
+    }
+
+    for( int j = 1; j <= 5*a; j++ ){
+       cout<< conteoFilas[j-1]<<" ";
+      if (j%5==0) {
+      cout<<endl;
+      }
+    }
+    for( int j = 1; j <= columnas; j++ ){
+       cout<< sumaColumnas[j-1]<<" ";
+    }
+  */
